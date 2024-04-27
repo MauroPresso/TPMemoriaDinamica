c413ab134529a710432cc77ea74c4de83568b85b
MauroPresso <mauro.presso@gmail.com>
2024-04-27 16:31:43 -0300

    Commiting lib.h from the folder TPMemoriaDinamica

---
+++ lib.h
@@ -0,0 +1,9 @@
+#pragma once
+
+int* resize(int* vec, int size, int new_size);
+
+int* removeItem(int* vec,int vec_size,int item_pos);
+
+int* insertItem(int* vec,int vec_size,int insert_pos,int insert_value);
+
+int* concat(int* vec_left,int left_size,int* vec_right,int right_size);
